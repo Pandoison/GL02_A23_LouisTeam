37231d9aad8b691d3065aa983cb91df9dfe62847
Pandoison <louis.hennechart@utt.fr>
2023-12-07 10:58:54 +0100

    Update main.js
    SPEC 3 et SPEC 7 qui marche bien

--- main.js
+++ main.js
@@ -66,6 +66,103 @@ cli
 		}
     })
 
+// Disponibilités d’une salle, SPEC 03
+.command('creneaux', "Affiche les créneaux pour lesquels une salle est libre")
+.argument('<salle>', "La salle de la recherche")
+.action(({args, options, logger}) => {
+    let analyseurFichier = recupererFichiers();
+
+    if (analyseurFichier.errorCount === 0) {
+        if (analyseurFichier.listeCreneaux.some(c => c.salle.match(args.salle))) {
+            var nomSalle = args.salle;
+            var creneauxOccupes = analyseurFichier.listeCreneaux.filter(c => c.salle.match(nomSalle));
+
+            // on initialise (6 jours, de 8h à 20h, créneaux de 30 minutes)
+            let creneauxDisponibles = [];
+            let jours = ["L", "MA", "ME", "J", "V", "S"];
+            jours.forEach(function (jour) {
+                if (jour !== "S") {
+                    for (var h = 8; h < 20; h++) {
+                        for (var min = 0; min < 60; min = min + 30) {
+                            if (min === 30) {
+                                creneauxDisponibles.push(new CreneauDisponible(jour, h + ":" + min, (h + 1) + ":" + "00"));
+                            } else {
+                                creneauxDisponibles.push(new CreneauDisponible(jour, h + ":" + "00", h + ":" + (min + 30)));
+                            }
+                        }
+                    }
+                } else {
+                    for (var h = 8; h < 12; h++) {
+                        for (var min = 0; min < 60; min = min + 30) {
+                            if (min === 30) {
+                                creneauxDisponibles.push(new CreneauDisponible(jour, h + ":" + min, (h + 1) + ":" + "00"));
+                            } else {
+                                creneauxDisponibles.push(new CreneauDisponible(jour, h + ":" + "00", h + ":" + (min + 30)));
+                            }
+                        }
+                    }
+                }
+            });
+
+            // On retire chaque créneau durant lequel la salle est occupéede a la liste représentant tous les créneaux  créés précédemment
+            creneauxOccupes.forEach(function (occupe) {
+                var debut = occupe.heureDebut.split(':');
+                var fin = occupe.heureFin.split(':');
+                var heureDebut = debut[0];
+                var minDebut = debut[1];
+                var heureFin = fin[0];
+                var minFin = fin[1];
+
+                if (minDebut === "30") {
+                    heureDebut = parseInt(heureDebut, 10) + 0.5;
+                }
+                if (minFin === "30") {
+                    heureFin = parseInt(heureFin, 10) + 0.5;
+                }
+
+                creneauxDisponibles.forEach(function (disponible) {
+                    if (disponible.jour === occupe.jour) {
+                        if (disponible.heureDebut === occupe.heureDebut) {
+                            var nbSuppression = (heureFin - heureDebut) * 2;
+                            creneauxDisponibles.splice(creneauxDisponibles.indexOf(disponible), nbSuppression);
+                        }
+                    }
+                });
+            });
+
+            // Affichage
+            jours.forEach(function (jour) {
+                var creneau = creneauxDisponibles.filter(c => c.jour.match(jour));
+                var creneaux = "";
+                creneau.forEach(function (c) {
+                    creneaux = creneaux + " " + c.heureDebut + "-" + c.heureFin;
+                });
+
+                if (jour === "L") {
+                    console.log("\nLundi : " + creneaux);
+                } else if (jour === "MA") {
+                    console.log("\nMardi : " + creneaux);
+                } else if (jour === "ME") {
+                    console.log("\nMercredi : " + creneaux);
+                } else if (jour === "J") {
+                    console.log("\nJeudi : " + creneaux);
+                } else if (jour === "V") {
+                    console.log("\nVendredi : " + creneaux);
+                } else if (jour === "S") {
+                    console.log("\nSamedi : " + creneaux);
+                }
+            });
+        } else {
+            logger.info("“Veuillez entrer un nom de salle valide".red)
+        }
+    } else {
+        logger.info("The .cru file contains error".red);
+    }
+})
+
+
+
+
     // Affiche les salles disponibles durant un créneau donné SPEC 04
     .command('sallesLibres', 'Consulter les salles libres durant un créneau donné')
     .argument('<heureDebut>', 'Heure du début du créneau avec 8:00=8h00; 8:30=8h30; 9:00= 9h00;...')
@@ -78,25 +175,25 @@ cli
             let horaires = ["8:00", "8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00"];
 
             if (horaires.includes(args.heureDebut) && horaires.includes(args.heureFin)) {
-                let analyzer = recupererFichiers();
+                let analyseurFichier = recupererFichiers();
 
                 // Initialiser les propriétés 'listeCreneaux' et 'reservations' si elles n'existent pas
-                if (!analyzer.listeCreneaux) {
-                   analyzer.listeCreneaux = [];
+                if (!analyseurFichier.listeCreneaux) {
+                   analyseurFichier.listeCreneaux = [];
                 }
-                if (!analyzer.reservations) {
-                    analyzer.reservations = [];
+                if (!analyseurFichier.reservations) {
+                    analyseurFichier.reservations = [];
                 }
 
-                if (analyzer.errorCount === 0) {
-                    if (!analyzer.listeCreneaux.isEmpty) {
+                if (analyseurFichier.errorCount === 0) {
+                    if (!analyseurFichier.listeCreneaux.isEmpty) {
                         let tableauSallesDisponibles = new Array();
-                        analyzer.listeCreneaux.forEach(c => creerTableauSallesVides(c, tableauSallesDisponibles, args.heureDebut, args.heureFin, args.jour));
+                        analyseurFichier.listeCreneaux.forEach(c => creerTableauSallesVides(c, tableauSallesDisponibles, args.heureDebut, args.heureFin, args.jour));
 
                         console.log("Voici la liste des salles disponibles :");
                         tableauSallesDisponibles.forEach(s => {
                             // Vérifier si la salle est réservée pendant le créneau
-                            let salleReservee = analyzer.reservations.some(r =>
+                            let salleReservee = analyseurFichier.reservations.some(r =>
                                 r.salle === s.salle &&
                                 r.jour === args.jour &&
                                 ((r.heureDebut <= s.heureDebut && s.heureDebut < r.heureFin) || (r.heureDebut < s.heureFin && s.heureFin <= r.heureFin)));// N'inclure la salle que si elle n'est pas réservée
@@ -117,6 +214,76 @@ cli
             logger.info("Le jour que vous avez entré n'est pas valide".red);
         }
     })
+
+
+
+
+// Visualisation synthétique du taux d’occupation, SPEC 07
+//Basé sur le nbr d'h doccupation dans la semaine par rapport à une semaine de 6 jours de 8h,  
+.command('tauxdOccupation', 'Crée un diagramme du taux d\'occupation')
+.action(({args, options, logger}) => {
+    let analyseurFichier = recupererFichiers();
+ 
+    if (analyseurFichier.errorCount === 0) {
+        var listeSalles = [];
+
+        analyseurFichier.listeCreneaux.forEach(function (creneau) {
+            var debut = creneau.heureDebut.split(':');
+            var fin = creneau.heureFin.split(':');
+            var heureDebut = debut[0];
+            var minDebut = debut[1];
+            var heureFin = fin[0];
+            var minFin = fin[1];
+
+            if (minDebut === "30") {
+                heureDebut = parseInt(heureDebut, 10) + 0.5;
+            }
+            if (minFin === "30") {
+                heureFin = parseInt(heureFin, 10) + 0.5;
+            }
+            const duree = heureFin - heureDebut;
+
+            if (listeSalles.some(e => e.nom.match(creneau.salle))) {
+                const salle = listeSalles.find(e => e.nom.match(creneau.salle));
+                const pos = listeSalles.indexOf(salle);
+                listeSalles[pos].occupation += duree;
+            } else {
+                listeSalles.push(new Salle(creneau.salle, duree))
+            }
+        });
+
+        listeSalles.forEach(function (salle) {
+            salle.occupation = (salle.occupation / 66) * 100
+        });
+
+        var occRtChart = {
+            "data": {
+                "values": listeSalles
+            },
+            "mark": "bar",
+            "encoding": {
+                "x": {"field": "nom", "type": "nominal"},
+                "y": {"field": "occupation", "type": "quantitative", "title": "Occupation Rate %"}
+            }
+        };
+
+        const myChart = vegalite.compile(occRtChart).spec;
+
+        var runtime = vg.parse(myChart);
+        var view = new vg.View(runtime).renderer('svg').run();
+        var mySvg = view.toSVG();
+        mySvg.then(function (res) {
+            fs.writeFileSync("./result.svg", res);
+            view.finalize();
+            logger.info("Chart output : ./result.svg");
+        });
+
+    } else {
+        logger.info("“Veuillez entrer un nom de salle valide".red);
+    }
+})
+
+
 
 
     //Affiche le classement des salles SPEC08
@@ -150,17 +317,17 @@ function recupererFichiers() {
     var dirFiles = browseDir.browseFiles(dataBasePath);
     let listFiles = [];
     dirFiles.forEach(e => listFiles.push(e.src));
-    let analyzer = new Parser();
+    let analyseurFichier = new Parser();
     if (listFiles.length !== 0) {
         for (var i = 0; i < listFiles.length; i++) {
             let data = fs.readFileSync(listFiles[i], 'utf-8');
-            analyzer.parse(data);
+            analyseurFichier.parse(data);
         }
     } else {
         console.log("Le fichier ".red + dataBasePath.red + " ne contient pas de fichiers .cru".red);
-        analyzer.errorCount++;
+        analyseurFichier.errorCount++;
     }
-    return analyzer;
+    return analyseurFichier;
 }
 
 
@@ -230,4 +397,17 @@ function creerTableauSallesDuCours(element, tab, cours){
 let objetSalle = function (nom, capMax){
 	this.nom = nom;
 	this.capMax = capMax;
+}
+
+// Objet créneau simplifié.
+let CreneauDisponible = function (jour, heureDebut, heureFin){
+	this.jour = jour;
+	this.heureDebut = heureDebut;
+	this.heureFin = heureFin;
+}
+
+// Objet salle avec un nombre d'heure d'occupation
+let Salle = function (nom, occupation){
+	this.nom = nom;
+	this.occupation = occupation;
 }
